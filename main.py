25091319e773c37b308e3c49290236ed2e1d8e8c
Victor B. <contact@itsvic.dev>
2025-12-26 06:25:40 +0100

    a

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import glob
+import hashlib
 import io
 import json
 import os
 import sys
+from os.path import dirname
+from urllib.parse import quote
 
 import pkg
 import sfo
@@ -20,11 +23,27 @@ pkgs_json_contents = []
 digests = set()
 
 
+def get_split_path(digest: str):
+    return f"{DATA_DIR}/{digest[0]}/{digest[1]}/{digest[2:]}"
+
+
+def get_hash_for_file(data: bytes) -> str:
+    md5obj = hashlib.md5()
+    md5obj.update(data)
+    return md5obj.hexdigest().upper()
+
+
 def find_by_title_id(title_id: str):
     for game in pkgs_json_contents:
         if game["titleID"] == title_id:
             return game
     return None
+
+
+def clean_ver(version) -> str:
+    if version is None:
+        raise Exception("PKG version is None")
+    return version.lstrip("0")
 
 
 for i in glob.iglob("**/*.pkg", recursive=True):
@@ -52,15 +71,24 @@ for i in glob.iglob("**/*.pkg", recursive=True):
         print("WARNING: digests aren't unique! duplicate file?")
     digests.add(str_digest)
 
-    data_dir = f"{DATA_DIR}/{str_digest[:1]}/{str_digest[:2]}/{str_digest}"
+    data_dir = get_split_path(str_digest)
     json_path = f"{data_dir}/install.json"
-    icon0_path = f"{data_dir}/icon0.png" if "icon0.png" in hdr.entries else None
-    pic1_path = f"{data_dir}/pic1.png" if "pic1.png" in hdr.entries else None
+    icon0_path = (
+        f"{get_split_path(get_hash_for_file(hdr.entries['icon0.png']))}/icon0.png"
+        if "icon0.png" in hdr.entries
+        else None
+    )
+    pic1_path = (
+        f"{get_split_path(get_hash_for_file(hdr.entries['pic1.png']))}/pic1.png"
+        if "pic1.png" in hdr.entries
+        else None
+    )
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir, exist_ok=True)
 
     if icon0_path is not None:
+        os.makedirs(dirname(icon0_path), exist_ok=True)
         with open(icon0_path, "wb+") as file:
             file.write(hdr.entries["icon0.png"])
 
@@ -72,7 +100,7 @@ for i in glob.iglob("**/*.pkg", recursive=True):
                 "numberOfSplitFiles": 1,
                 "pieces": [
                     {
-                        "url": f"{root_url}/{i}",
+                        "url": f"{root_url}/{quote(i)}",
                         "fileOffset": 0,
                         "fileSize": os.lstat(i).st_size,
                         "hashValue": "0000000000000000000000000000000000000000",
@@ -85,13 +113,13 @@ for i in glob.iglob("**/*.pkg", recursive=True):
     game["pkgs"].append(
         {
             "name": sfo_data["TITLE"],
-            "pkgPath": f"{root_url}/{i}",
-            "jsonPath": f"{root_url}/{json_path}",
-            "icon0Path": f"{root_url}/{icon0_path}" if icon0_path is not None else None,
+            "pkgPath": f"{root_url}/{quote(i)}",
+            "jsonPath": f"{root_url}/{quote(json_path)}",
+            "icon0Path": f"{root_url}/{quote(icon0_path)}"
+            if icon0_path is not None
+            else None,
             "size": os.lstat(i).st_size,
-            "originalSize": hdr.original_size,
-            "digest": str_digest,
-            "version": sfo_data.get("APP_VER", sfo_data.get("VERSION")),
+            "version": clean_ver(sfo_data.get("APP_VER", sfo_data.get("VERSION"))),
             "type": "PS4GD"
             if hdr.content_type == pkg.PKG_CONTENT_TYPE_GD
             else "PS4AC"
@@ -108,16 +136,14 @@ for i in glob.iglob("**/*.pkg", recursive=True):
     # these might be incorrect due to a DLC loading first. let's correct just in case
     if hdr.content_type == pkg.PKG_CONTENT_TYPE_GD and icon0_path:
         game["name"] = sfo_data["TITLE"]
-        game["icon0_path"] = f"{root_url}/{icon0_path}"
+        game["icon0Path"] = f"{root_url}/{quote(icon0_path)}"
 
     if hdr.content_type == pkg.PKG_CONTENT_TYPE_GD and pic1_path:
-        game["pic1_path"] = f"{root_url}/{pic1_path}"
+        game["pic1Path"] = f"{root_url}/{quote(pic1_path)}"
         # write pic1
+        os.makedirs(dirname(pic1_path), exist_ok=True)
         with open(pic1_path, "wb+") as file:
             file.write(hdr.entries["pic1.png"])
-
-    sfo_data = sfo.read_sfo(io.BytesIO(hdr.entries["param.sfo"]))
-    version = sfo_data.get("APP_VER", sfo_data.get("VERSION"))
 
 with open("pkgs.json", "w+") as file:
     json.dump(pkgs_json_contents, file, indent=2)
